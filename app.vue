93bc3f990c95640ed038afd7a5e4615ec4864513
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-02-06 16:10:10 +0300

    destop with movies queries

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg-slate-900 min-h-screen">
+    <Navbar />
+    <NuxtPage />
+    <Foot />
+  </div>
+</template>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Alegreya+Sans:wght@100;300;400;500;700;800;900&display=swap");
+
+* {
+  font-family: "Alegreya Sans", sans-serif;
+}
+</style>
